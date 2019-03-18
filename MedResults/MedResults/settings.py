eaadb4ee48c4db8301bcc56e1f1c5b3db4492004
crbrk <cbrk@protonmail.com>
2019-03-18 23:59:22 +0100

    project_dir, static_root

--- MedResults/MedResults/settings.py
+++ MedResults/MedResults/settings.py
@@ -129,7 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 
 ]
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 #MEDIA_URL is a placeholder for the url a client should hit to access your media.
 MEDIA_URL = '/media/documents/'
